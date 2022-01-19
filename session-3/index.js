ad32f4171768f326a5ba933864eec892ed38e74e
Rose <roselle30@hotmail.com>
2022-01-19 19:16:38 +0000

    changed variables

--- session-3/index.js
+++ session-3/index.js
@@ -1,2 +1,9 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
+var a = 5;
+var b = 10;
+var c = a + b;
+
+let a = 5;
+const b = 10;
+let c = a + b;
